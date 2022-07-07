c19fd023d2e35b2b76c48b45843d6c18f5d62dd6
Justin Stewart <justinstewart.la@gmail.com>
2022-07-06 22:40:10 -0400

    updated botname

--- ef_instagram_scraper/settings.py
+++ ef_instagram_scraper/settings.py
@@ -4,7 +4,7 @@ AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 
 # Main Scrapy Configs
-BOT_NAME = 'artsy'
+BOT_NAME = 'instagram'
 SPIDER_MODULES = ['ef_instagram_scraper.spiders']
 NEWSPIDER_MODULE = 'ef_instagram_scraper.spiders'
 CONCURRENT_REQUESTS = 64
