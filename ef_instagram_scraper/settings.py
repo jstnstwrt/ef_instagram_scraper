07b51b34e7b620eec9b7a5409dda207ca9f22e54
Justin Stewart <justinstewart.la@gmail.com>
2022-07-07 21:34:53 -0400

    updated concurrency to 1

--- ef_instagram_scraper/settings.py
+++ ef_instagram_scraper/settings.py
@@ -7,7 +7,7 @@ AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 BOT_NAME = 'instagram'
 SPIDER_MODULES = ['ef_instagram_scraper.spiders']
 NEWSPIDER_MODULE = 'ef_instagram_scraper.spiders'
-CONCURRENT_REQUESTS = 64
+CONCURRENT_REQUESTS = 1
 
 ## S3 Filepath and Export Feed
 FEED_URI = 's3://euclidsfund-data-pipeline/data_acquisition/instagram/raw/%(name)s/%(time)s.json'
